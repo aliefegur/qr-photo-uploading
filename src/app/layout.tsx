74c94d089d175b0c02b4e15b4ac7f97cab14e802
aliefegur <ali.efe.gur@hotmail.com>
2025-08-30 09:27:20 +0300

    Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type {Metadata} from 'next';
 import {Rubik} from 'next/font/google';
 import {ReactNode} from "react";
 import './globals.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEnvelope, faGlobe, faPhone} from "@fortawesome/free-solid-svg-icons";
+import Image from "next/image";
 
 const rubik = Rubik({
   variable: '--font-rubik',
@@ -20,7 +23,36 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="tr">
-    <body className={`${rubik.className} antialiased`}>{children}</body>
+    <body className={`${rubik.className} antialiased`}>
+    {children}
+
+    <footer
+      className={`${rubik.className} px-8 py-8 bg-gray-400 flex flex-col gap-6`}>
+      <div className="flex flex-col items-baseline max-md:gap-6 md:flex-row md:items-start md:justify-center gap-6">
+        <Image src={"/davetiye.jpg"} alt={"Düğün davetiyesi"} width={512} height={512}
+               className="max-h-[80vh] object-contain"/>
+        <div className="flex flex-col gap-1">
+          <h6 className="font-medium text-lg">İletişim</h6>
+          <a href="https://aliefegur.com" target="_blank" rel="noopener noreferrer"
+             className="flex items-center gap-1">
+            <FontAwesomeIcon icon={faGlobe}/>
+            <p>aliefegur.com</p>
+          </a>
+          <a href="mailto:ali.efe.gur@hotmail.com" target="_blank" rel="noopener noreferrer"
+             className="flex items-center gap-1">
+            <FontAwesomeIcon icon={faEnvelope}/>
+            <p>ali.efe.gur@hotmail.com</p>
+          </a>
+          <a href="tel:+905550232004" target="_blank" rel="noopener noreferrer"
+             className="flex items-center gap-1">
+            <FontAwesomeIcon icon={faPhone}/>
+            <p>+90 555 023 2004</p>
+          </a>
+          <p className="text-center text-sm font-light mt-6">Ali Efe GÜR © 2025 - Tüm Hakları Saklıdır</p>
+        </div>
+      </div>
+    </footer>
+    </body>
     </html>
   );
 }
